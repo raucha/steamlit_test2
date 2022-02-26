a799a75af81a6a200745d018611e7e8da834a850
raucha <raucha@users.noreply.github.com>
2022-02-26 19:22:38 +0000

    init

---
+++ example.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+
+
+def main():
+    # Streamlit が対応している任意のオブジェクトを可視化する (ここでは文字列)
+    st.write('Hello, World!')
+
+
+if __name__ == '__main__':
+    main()
